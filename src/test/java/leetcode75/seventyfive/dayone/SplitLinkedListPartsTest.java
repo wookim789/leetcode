b3b92131b68316242feb66b8b14bacd6668270ef
wookim789 <wookim789@gmail.com>
2023-01-03 17:29:01 +0900

    test code 추가

---
+++ src/test/java/leetcode75/seventyfive/dayone/SplitLinkedListPartsTest.java
@@ -0,0 +1,34 @@
+package leetcode75.seventyfive.dayone;
+
+import leetcode75.seventyfive.dayone.datatype.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SplitLinkedListPartsTest {
+
+    private ListNode createNode(int firstNum, int lastNum) {
+        ListNode listNode = new ListNode(firstNum);
+        ListNode nextNode = listNode;
+
+        for (int i = firstNum + 1; i < lastNum + 1; i++) {
+            nextNode.next = new ListNode(i);
+            nextNode = nextNode.next;
+        }
+        return listNode;
+    }
+
+    @Test
+    void splitListToPartsTest() {
+        // given
+        ListNode testNode = createNode(1, 10);
+        int k = 3;
+
+        // when
+        ListNode[] actuals = SplitLinkedListParts.splitListToParts(testNode, k);
+
+        // then
+        ListNode[] expecteds = new ListNode[]{createNode(1, 4), createNode(5, 7), createNode(8, 10)};
+        assertThat(actuals).usingRecursiveComparison().isEqualTo(expecteds);
+    }
+}
