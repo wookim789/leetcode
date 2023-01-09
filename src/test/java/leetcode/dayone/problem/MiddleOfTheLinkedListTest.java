f083ad07b4af5b3ef4ccba683156c528019f786e
wookim789 <wookim789@gmail.com>
2023-01-10 01:15:11 +0900

    master MiddleOfTheLinkedList solve

--- src/test/java/leetcode/dayone/problem/MiddleOfTheLinkedListTest.java
+++ src/test/java/leetcode/dayone/problem/MiddleOfTheLinkedListTest.java
@@ -2,7 +2,6 @@ package leetcode.dayone.problem;
 
 
 import leetcode.dayone.datatype.ListNode;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
